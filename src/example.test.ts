55a213e04007c0b711e765c49fc4ed442009527a
Michael L Perry <michael@qedcode.com>
2025-11-24 19:33:39 -0600

    Scaffold TypeScript and Jest

---
+++ src/example.test.ts
@@ -0,0 +1,11 @@
+import { add } from './example';
+
+describe('example', () => {
+  it('should add two numbers', () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  it('should handle negative numbers', () => {
+    expect(add(-1, 1)).toBe(0);
+  });
+});
